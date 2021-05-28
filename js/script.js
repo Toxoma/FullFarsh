955191d1d2b081307dff88e318b33d84b67bedc1
Daniil Veselov <daniil.veselov2012@yandex.ru>
2021-05-28 10:28:15 +0300

    lesson2

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
-let money = 100;
-let income = 100;
-let addExpenses = 100;
-let deposit = 100;
-let mission = 100;
-let period = 100;
+"use strict";
 
-alert('Это первое дз!');
-console.log('Это первое дз!');
+let money = 300; //“Доход за месяц”
+let income = 'фриланс';
+let addExpenses = 'интернет, Такси, коммуналка';
+let deposit = true;
+let mission = 3000;
+let period = 10;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log(`Период равен ${period} месяцев`);
+console.log('Цель заработать ' + mission + ' рублей');
+
+addExpenses.toLowerCase().split(', ').forEach(item => console.log(item));
+
+const budgetDay = money / 30;
+console.log(budgetDay);
