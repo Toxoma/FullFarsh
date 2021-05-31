5e066d22118a0debcb5b724944fc203009ef0bba
Daniil Veselov <daniil.veselov2012@yandex.ru>
2021-05-31 10:16:07 +0300

    lesson3

--- js/script.js
+++ js/script.js
@@ -16,5 +16,35 @@ console.log('Цель заработать ' + mission + ' рублей');
 
 addExpenses.toLowerCase().split(', ').forEach(item => console.log(item));
 
-const budgetDay = money / 30;
-console.log(budgetDay);
+money = prompt('Ваш месячный доход?');
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+deposit = confirm("Есть ли у вас депозит в банке?");
+let expenses1 = prompt('Введите 1ую обязательную статью расходов?');
+let amount1 = prompt('1) Во сколько это обойдется??');
+let expenses2 = prompt('Введите 2ую обязательную статью расходов?');
+let amount2 = prompt('2) Во сколько это обойдется??');
+
+let budgetMonth = +money - +amount1 - +amount2;
+console.log('budgetMonth: ', budgetMonth);
+
+console.log("Цель будет достигнута за " + Math.ceil(mission / budgetMonth) + " месяцев");
+
+let budgetDay = budgetMonth / 30;
+console.log('budgetDay: ', Math.floor(budgetDay));
+
+//! 1ый варик
+if (budgetDay > 1200) {
+   console.log('У вас высокий уровень дохода');
+}
+if (budgetDay > 600 && budgetDay <= 1200) {
+   console.log('У вас средний уровень дохода');
+}
+if (budgetDay <= 600 && budgetDay > 0) {
+   console.log('К сожалению у вас уровень дохода ниже среднего');
+}
+if (budgetDay <= 0) {
+   console.log('Что то пошло не так');
+}
+//! 2ой варик
+console.log(budgetDay <= 0 ? 'Что то пошло не так' : budgetDay <= 600 && budgetDay > 0 ? 'К сожалению у вас уровень дохода ниже среднего' : budgetDay > 600 && budgetDay <= 1200 ? 'У вас средний уровень дохода' : 'У вас высокий уровень дохода');
+
