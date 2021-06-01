e0cdefae2b08a767a078c7b6230b54edc8e9bd99
Daniil Veselov <daniil.veselov2012@yandex.ru>
2021-06-01 20:19:05 +0300

    rewrite

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 "use strict";
 
+let money = prompt('Ваш месячный доход?');
+let income = 'фриланс';
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'интернет, Такси, коммуналка');
+let deposit = confirm("Есть ли у вас депозит в банке?");
+let expenses1 = prompt('Введите 1ую обязательную статью расходов?');
+let amount1 = prompt('1) Во сколько это обойдется??');
+let expenses2 = prompt('Введите 2ую обязательную статью расходов?');
+let amount2 = prompt('2) Во сколько это обойдется??');
+let mission = 3000;
+let period = 10;
+const minus = getExpensesMonth(+amount1, +amount2);
+const accumulatedMonth = getAccumulatedMonth(money, minus);
+let budgetDay = Math.floor(accumulatedMonth / 30);
+
+
 function getExpensesMonth(a, b) {
    return a + b;
 }
@@ -26,20 +41,6 @@ function getStatusIncome(a) {
       return 'Что то пошло не так';
    }
 }
-
-let money = prompt('Ваш месячный доход?');
-let income = 'фриланс';
-let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'интернет, Такси, коммуналка');
-let deposit = confirm("Есть ли у вас депозит в банке?");
-let expenses1 = prompt('Введите 1ую обязательную статью расходов?');
-let amount1 = prompt('1) Во сколько это обойдется??');
-let expenses2 = prompt('Введите 2ую обязательную статью расходов?');
-let amount2 = prompt('2) Во сколько это обойдется??');
-let mission = 3000;
-let period = 10;
-const minus = getExpensesMonth(+amount1, +amount2);
-const accumulatedMonth = getAccumulatedMonth(money, minus);
-let budgetDay = Math.floor(accumulatedMonth / 30);
 
 showTypeOf(+money);
 showTypeOf(income);
